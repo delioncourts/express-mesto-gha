de8e10bb25d5b4764ab3dc1ed863ad522ea77ac9
delioncourts <delioncourtles@yandex.ru>
2022-05-15 20:52:17 +0300

    upd eslint and package json

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'eslint:recommended',
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'no-underscore-dangle': ['error', {'allow': ['_id']}]
+  },
+};
