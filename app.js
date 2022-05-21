a7b43e1b7b148da9caaef59ba01e1922eee0dfcb
delioncourts <delioncourtles@yandex.ru>
2022-05-21 16:49:21 +0300

    upd app

--- app.js
+++ app.js
@@ -5,23 +5,10 @@ const app = express();
 const { PORT = 3000 } = process.env;
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
+
 app.use(express.json());
 
 app.use('/users', require('./routes/users'));
-
-/*
-app.use('/users/:id', (req, res) => {
-    const id = req.params.id
-})
-
-app.use('/', (req, res) => {
-    res.status(200).send('all ok')
-})
-
-app.use('/', (req, res) => {
-    res.status(200).send('all ok')
-})*/
-
 
 app.listen(PORT, () => {
 console.log('server is set')});
