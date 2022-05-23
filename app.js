2eb0ca9a1e2bfc399d008ce2d414fb3febf1de0c
delioncourts <delioncourtles@yandex.ru>
2022-05-23 23:14:17 +0300

    upd app js

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', require('./routes/users'));
 
+app.use((req, res, next) => {
+  req.user = {
+    _id: '628bc7ccd7ad43c895a4447e', // вставьте сюда _id созданного в предыдущем пункте пользователя
+  };
+
+  next();
+});
+
 app.listen(PORT, () => {
   console.log('server is set');
 });
