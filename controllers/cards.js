a3a2fd73930771bc2b421b79bed0ca5d3256e1ba
delioncourts <delioncourtles@yandex.ru>
2022-05-26 23:38:47 +0300

    upd cards (add like and dislike and in routers)

--- controllers/cards.js
+++ controllers/cards.js
@@ -14,10 +14,9 @@ const createCard = (req, res) => {
     .then((card) => res.send({ data: card }))
     .catch((err) => {
       if (err.name === 'CastError') {
-        res.status(400).send({ message: 'Некорректные данные' });
-        return;
+        return res.status(400).send({ message: 'Некорректные данные' });
       }
-      res.status(500).send({ message: 'Ошибка на сервере' });
+      return res.status(500).send({ message: 'Ошибка на сервере' });
     });
 };
 
@@ -25,17 +24,58 @@ const deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
     .then((card) => {
       if (!card) {
-        res.status(404).send({ message: 'Карточка не найдена' });
-        return;
+        return res.status(404).send({ message: 'Карточка не найдена' });
       }
-      res.send({ data: card });
+      return res.send({ data: card });
     })
     .catch((err) => {
       if (err.name === 'CastError') {
-        res.status(400).send({ message: 'Некорректные данные id' });
-        return;
+        return res.status(400).send({ message: 'Некорректные данные id' });
       }
-      res.status(500).send({ message: 'Ошибка на сервере' });
+      return res.status(500).send({ message: 'Ошибка на сервере' });
     });
 };
-module.exports = { getCards, createCard, deleteCard };
+
+const likeCard = (req, res) => {
+  Card.findByIdAndRemove(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
+    { new: true },
+  )
+    .then((card) => {
+      if (!card) {
+        return res.status(404).send({ message: 'Карточка не найдена' });
+      }
+      return res.send({ data: card });
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(400).send({ message: 'Некорректные данные id' });
+      }
+      return res.status(500).send({ message: 'Ошибка на сервере' });
+    });
+};
+
+const dislikeCard = (req, res) => {
+  Card.findByIdAndRemove(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } }, // убрать _id из массива
+    { new: true },
+  )
+    .then((card) => {
+      if (!card) {
+        return res.status(404).send({ message: 'Карточка не найдена' });
+      }
+      return res.send({ data: card });
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(400).send({ message: 'Некорректные данные id' });
+      }
+      return res.status(500).send({ message: 'Ошибка на сервере' });
+    });
+};
+
+module.exports = {
+  getCards, createCard, deleteCard, likeCard, dislikeCard,
+};
