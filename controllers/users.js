a340e81c8079c721b19d87125501a13098469218
delioncourts <delioncourtles@yandex.ru>
2022-05-26 23:27:43 +0300

    upd users(add update+avatar change), routers for them

--- controllers/users.js
+++ controllers/users.js
@@ -10,17 +10,15 @@ const getUserById = (req, res) => {
   User.findById(req.params.userId)
     .then((users) => {
       if (!users) {
-        res.status(404).send({ message: 'Пользователь не найден' });
-        return;
+        return res.status(404).send({ message: 'Пользователь не найден' });
       }
       res.send({ data: users });
     })
     .catch((err) => {
       if (err.name === 'CastError') {
-        res.status(400).send({ message: 'Некорректные данные id' });
-        return;
+        return res.status(400).send({ message: 'Некорректные данные id' });
       }
-      res.status(500).send({ message: 'Ошибка на сервере' });
+      return res.status(500).send({ message: 'Ошибка на сервере' });
     });
 };
 
@@ -32,8 +30,52 @@ const createUsers = (req, res) => {
       if (err.name === 'ValidationError') {
         return res.status(400).send({ message: 'Некорректные данные пользователя' });
       }
-      res.status(500).send({ message: 'Ошибка на сервере' });
+      return res.status(500).send({ message: 'Ошибка на сервере' });
     });
 };
 
-module.exports = { getUsers, getUserById, createUsers };
+const updateUser = (req, res) => {
+  const { name, about } = req.body;
+  return User.findByIdAndUpdate(
+    req.user._id,
+    { name, about },
+    { new: true, runValidators: true, upsert: false },
+  )
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({ message: 'Пользователь не найден' });
+      }
+      res.send({ data: user });
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: 'Некорректные данные пользователя' });
+      }
+      return res.status(500).send({ message: 'Ошибка на сервере' });
+    });
+};
+
+const updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+  return User.findByIdAndUpdate(
+    req.user._id,
+    { avatar },
+    { new: true, runValidators: true, upsert: false },
+  )
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({ message: 'Пользователь не найден' });
+      }
+      res.send({ data: user });
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: 'Некорректные данные пользователя' });
+      }
+      return res.status(500).send({ message: 'Ошибка на сервере' });
+    });
+};
+
+module.exports = {
+  getUsers, getUserById, createUsers, updateUser, updateAvatar,
+};
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
 
-const { getUsers, getUserById, createUsers } = require('../controllers/users');
+const {
+  getUsers, getUserById, createUsers, updateUser, updateAvatar,
+} = require('../controllers/users');
 
 router.get('/users', getUsers);
 router.get('/users/:userId', getUserById);
 router.post('/users', createUsers);
+router.patch('/users/me', updateUser);
+router.patch('/users/me/avatar', updateAvatar);
 
 module.exports = router;
